be09484534d0c5048311c9172551789502e0d85d
sashatsyhanov <alexandertsyhanov@gmail.com>
2026-05-28 22:40:21 +0300

    chore: trigger redeploy after env vars setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* config options here - trigger build after env update */
 };
 
 export default nextConfig;
