f9702ba6cb5a9188af4e1a47de7c9fc5c3d39165
sashatsyhanov <alexandertsyhanov@gmail.com>
2026-06-06 16:57:16 +0300

    fix: Explicitly assign gtag to window.gtag to fix GA tracking in Next.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -97,9 +97,9 @@ export default function RootLayout({
             <Script id="ga-init" strategy="afterInteractive">
               {`
                 window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${GA_ID}', {
+                window.gtag = function(){window.dataLayer.push(arguments);}
+                window.gtag('js', new Date());
+                window.gtag('config', '${GA_ID}', {
                   page_path: window.location.pathname,
                   send_page_view: true
                 });
